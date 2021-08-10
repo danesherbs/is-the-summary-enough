08eea62ffdad7e9b0b6aec557a92ca927a332690
danesherbs-tw <danesherbs@gmail.com>
2021-08-11 09:38:37 +1000

    Update schema and add voting logic

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS is_summary_enough (
-	book_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS books (
+	id INTEGER PRIMARY KEY,
 	title TEXT NOT NULL,
 	author TEXT NOT NULL,
   votes_yes INTEGER NOT NULL,
   votes_no INTEGER NOT NULL
 );
 
-INSERT INTO is_summary_enough VALUES( 0,	"Stubborn Attachments", "Tyler Cowen", 2, 19);
-INSERT INTO is_summary_enough VALUES( 1,	"The Art of Doing Science and Engineering", "Richard Hamming", 1, 10);
-INSERT INTO is_summary_enough VALUES( 2,	"Never Split the Difference", "Chris Voss", 10, 1);
+INSERT INTO books VALUES( 0,	"Stubborn Attachments", "Tyler Cowen", 2, 19);
+INSERT INTO books VALUES( 1,	"The Art of Doing Science and Engineering", "Richard Hamming", 0, 0);
+INSERT INTO books VALUES( 2,	"Never Split the Difference", "Chris Voss", 10, 1);
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const sqlite3 = require("sqlite3").verbose();
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const db = new sqlite3.Database("./database.db");
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/search", (req, res) => {
   const query = req.query.query || "";
 
   if (query.length === 0) {
@@ -27,7 +33,7 @@ app.get("/", (req, res) => {
     return { ...book, href: `book/${book.id}` };
   });
 
-  res.render("results", {
+  res.render("search", {
     query: query,
     count: results.length,
     results: results,
@@ -35,23 +41,69 @@ app.get("/", (req, res) => {
 });
 
 app.get("/book/:id", (req, res) => {
-  const sql = `SELECT * FROM is_summary_enough WHERE book_id = ${req.params.id}`;
+  const bookId = req.params.id;
+  const hasVoted = req.cookies ? req.cookies[bookId] !== undefined : false;
+  const sql = `SELECT * FROM books WHERE id = ${req.params.id}`;
 
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
     }
 
+    // TODO: create db entry if book doesn't exist
+
     if (rows.length !== 1) {
       return res.send("Sorry there was an error retrieving the book id!");
     }
 
     const [book] = rows;
+    const totalVotes = book.votes_yes + book.votes_no;
+    const percentageYes =
+      totalVotes === 0
+        ? 0
+        : Math.round(
+            (100 * book.votes_yes) / (book.votes_yes + book.votes_no),
+            0
+          );
+    const percentageNo = totalVotes === 0 ? 0 : 100 - percentageYes;
 
     res.render("book", {
       book: book,
+      percentageYes: percentageYes,
+      percentageNo: percentageNo,
+      hasVoted: hasVoted,
     });
   });
+});
+
+app.get("/vote/book/:id/yes", (req, res) => {
+  const bookId = parseInt(req.params.id);
+
+  if (bookId !== NaN) {
+    const hasVoted = req.cookies ? req.cookies[bookId] !== undefined : false;
+
+    if (!hasVoted) {
+      res.cookie(req.params.id, 1, { httpOnly: true });
+      db.run(`UPDATE books SET votes_yes = votes_yes + 1 WHERE id = ${bookId}`);
+    }
+  }
+
+  res.send();
+});
+
+app.get("/vote/book/:id/no", (req, res) => {
+  const bookId = parseInt(req.params.id);
+
+  if (bookId !== NaN) {
+    const hasVoted = req.cookies ? req.cookies[bookId] !== undefined : false;
+
+    if (!hasVoted) {
+      res.cookie(req.params.id, 0);
+      db.run(`UPDATE books SET votes_no = votes_no + 1 WHERE id = ${bookId}`);
+    }
+  }
+
+  res.send();
 });
 
 app.listen(process.env.PORT || 3000);
