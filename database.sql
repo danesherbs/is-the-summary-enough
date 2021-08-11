c5847b92f4183783bdfd303a34d6c353af63563a
danesherbs-tw <danesherbs@gmail.com>
2021-08-11 18:30:46 +1000

    Update database

--- database.sql
+++ database.sql
@@ -5,7 +5,3 @@ CREATE TABLE IF NOT EXISTS books (
   votes_yes INTEGER NOT NULL,
   votes_no INTEGER NOT NULL
 );
-
-INSERT INTO books VALUES( "k8csuh",	"Stubborn Attachments", "Tyler Cowen", 2, 19);
-INSERT INTO books VALUES( "s8fwhq",	"The Art of Doing Science and Engineering", "Richard Hamming", 0, 0);
-INSERT INTO books VALUES( "cv9wdb",	"Never Split the Difference", "Chris Voss", 10, 1);
